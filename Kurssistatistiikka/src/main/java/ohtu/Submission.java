9753524f35585f3d2b33d7ed9b291a2cff8f1c48
Hansuzu <hannes.ihalainen@gmail.com>
2018-11-16 11:03:30 +0200

    task 1

--- Kurssistatistiikka/src/main/java/ohtu/Submission.java
+++ Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -1,7 +1,12 @@
 package ohtu;
 
+import java.util.ArrayList;
+
 public class Submission {
     private int week;
+    private int hours;
+    private String course;
+    private ArrayList<Integer> exercises;
 
     public void setWeek(int week) {
         this.week = week;
@@ -11,9 +16,31 @@ public class Submission {
         return week;
     }
 
+    public int getNumberOfExercises(){
+        return exercises.size();
+    }
+
+    public int getHours(){
+        return hours;
+    }
+
+    private String exerciseString(){
+        String v="";
+        for (int i=0; i<exercises.size(); i++) {
+            if (i != 0) {
+                v+=", ";
+            }
+            v+=""+exercises.get(i);
+        }
+        return v;
+    }
     @Override
     public String toString() {
-        return ""+week;
+        return course
+                +", viikko "+week
+                +" tehtyjä tehtäviä yhteensä "+exercises.size()
+                +" aikaa kului "+hours
+                +" tehdyt tehtävät: "+exerciseString();
     }
     
 }
